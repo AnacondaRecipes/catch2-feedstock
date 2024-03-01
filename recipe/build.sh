df53276d6ec6d324a429bf5dea9516af85d352f0
Jack Olivieri <jolivieri@anaconda.com>
2024-03-01 22:37:52 +0100

    revert recipe to 2.6.0 at initial commit

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,23 +5,14 @@ set -e
 mkdir build
 cd build
 
-# macOS: allow newer features
-# https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-
-cmake ${CMAKE_ARGS} \
-  -LAH -G"Ninja" \
+cmake -LAH -G"Ninja" \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DCMAKE_INSTALL_LIBDIR=lib \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCATCH_ENABLE_WERROR:BOOL=OFF \
   ..
 
 # build and install
 cmake --build . --target install
 
 # test
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
-  ctest -R
-fi
+ctest -R
