a83c20d7e0c08fe554ca28996870acf5ff160ec4
Melody Love <mlove@anaconda.com>
2024-03-07 10:50:11 -0700

    Fix variable call in bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,7 @@ setlocal EnableDelayedExpansion
 :: configure
 cmake -Bbuild -H. ^
   -DBUILD_TESTING=OFF ^
-  -DCMAKE_INSTALL_PREFIX=$PREFIX
+  -DCMAKE_INSTALL_PREFIX=%PREFIX%
 if errorlevel 1 exit 1
 
 :: build and install
